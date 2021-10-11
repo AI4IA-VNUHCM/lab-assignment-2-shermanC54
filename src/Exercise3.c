ea2787d4a2b91be37faf69b4259630edb6815a18
shermanC54 <ITITIU19007@student.hcmiu.edu.vn>
2021-10-11 15:01:10 +0700

    lab 02

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -15,7 +15,23 @@ int main(int argc, char *argv[]) {
 	int testcase = atoi(argv[1]);
 	
 	//Your codes here
-	
-	
+	int n;
+	n = testcase;
+	while (n%2 == 0)
+    {
+        printf("%d ", 2);
+        n = n/2;
+    }
+ 	for (int i = 3; i <= sqrt(n); i = i+2)
+    {
+       while (n%i == 0)
+        {
+            printf("%d ", i);
+            n = n/i;
+        }
+    }
+    if (n > 2)
+        printf ("%d ", n);
+
 	return 0;
 }
